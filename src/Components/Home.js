b4ae439d472d3c114edbbac3988e78003a56755c
hassan <hassan.elouardy06@gmail.com>
2023-07-30 23:13:47 +0100

    Add logout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import "../index.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    if (!username) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
   return (
-    <div>
+    <div className="home">
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={handleLogout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <h2>Welcome to the Dashboard</h2>
     </div>
   );
