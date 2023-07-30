ac38eb1d69e6df2a85c89bfcfd5d3563d1e33128
hassan <hassan.elouardy06@gmail.com>
2023-07-30 13:04:21 +0100

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App">Hello!</div>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
