fde08ff1b2d243cb6168f1416fcb003ecf31c448
hassan <hassan.elouardy06@gmail.com>
2023-07-30 16:36:12 +0100

    Add validation to register

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Register = () => {
   const [userName, SetUserName] = useState("");
   const [email, SetEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleRegister(e) {
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
     e.preventDefault();
     console.log({ userName, email, password });
+    if (validate()) {
+      toast.success("Registered successfully");
+      navigate("/login");
+    }
+
     SetUserName("");
     SetEmail("");
     setPassword("");
-  }
+  };
+
+  const validate = () => {
+    let result = true;
+    let errorMessage = "Please enter the value in";
+    if (userName === "" || userName === null) {
+      result = false;
+      errorMessage += " Username";
+    }
+    if (email === "" || email === null) {
+      result = false;
+      errorMessage += " Email";
+    }
+    if (password === "" || password === null) {
+      result = false;
+      errorMessage += " Password";
+    }
+
+    if (!result) {
+      toast.warning(errorMessage);
+    }
+    return result;
+  };
 
   return (
-    <form onClick={handleRegister}>
+    <form onSubmit={handleRegister}>
       <h2>Register </h2>
       <div>
         <div>
