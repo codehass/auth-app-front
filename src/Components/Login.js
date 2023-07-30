9cf8131d9032a8dd88cb3acc72b0c60d8f7050cf
hassan <hassan.elouardy06@gmail.com>
2023-07-30 21:03:53 +0100

    Add token to the local storage

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import "../assets/register.css";
 
 const Login = () => {
   const [userName, SetUserName] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
+
+  const fakeAPICall = (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const token = "fake-jwt-token";
+
+        resolve(token);
+      }, 1000);
+    });
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
     if (validate()) {
-      toast.success("Registered successfully");
-      navigate("/");
+      fakeAPICall({
+        username: userName,
+        password: password,
+      })
+        .then((token) => {
+          localStorage.setItem("jwtToken", token);
+          toast.success("Login successful!", {
+            position: toast.POSITION.TOP_CENTER,
+          });
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log("Error during login:", error);
+          toast.error("Invalid credentials. Please try again.", {
+            position: toast.POSITION.TOP_CENTER,
+          });
+        });
     }
   };
 
@@ -36,30 +61,38 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <h2>Login </h2>
+    <form class="register" onSubmit={handleLogin}>
+      <h2 className="form-title">Login </h2>
       <div>
-        <div>
+        <div class="group-input">
           <label>User Name</label>
           <input
             type="text"
             placeholder="Username"
             value={userName}
             onChange={(e) => SetUserName(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
+        <div class="group-input">
           <label>Password</label>
           <input
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="input"
           />
         </div>
       </div>
-      <button type="submit">Login</button>
-      <Link to="/register">Register</Link>
+      <button className="btn" type="submit">
+        Login
+      </button>
+
+      <div className="link">
+        <p class="signup-link"> No account?</p>
+        <Link to="/register">Register</Link>
+      </div>
     </form>
   );
 };
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import "../assets/register.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const { v4: uuidv4 } = require("uuid");
 
 const Register = () => {
   const [userName, SetUserName] = useState("");
   const [email, SetEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
+
+  const fakeAPICall = (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const token = uuidv4();
+        resolve(token);
+      }, 1000);
+    });
+  };
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log({ userName, email, password });
+
+    // console.log({ userName, email, password });
     if (validate()) {
-      toast.success("Registered successfully");
-      navigate("/login");
+      toast.success("Registered successfully", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+
+      fakeAPICall({
+        username: userName,
+        email: email,
+        password: password,
+      })
+        .then((token) => {
+          localStorage.setItem("jwtToken", token);
+          navigate("/login");
+        })
+        .catch((error) => {
+          console.log("Error during registration:", error);
+        });
     }
 
     SetUserName("");
@@ -39,45 +66,55 @@ const Register = () => {
     }
 
     if (!result) {
-      toast.warning(errorMessage);
+      toast.warning(errorMessage, {
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
     return result;
   };
 
   return (
-    <form onSubmit={handleRegister}>
-      <h2>Register </h2>
+    <form class="register" onSubmit={handleRegister}>
+      <h2 className="form-title">Register </h2>
       <div>
-        <div>
+        <div class="group-input">
           <label>User Name</label>
           <input
             type="text"
             placeholder="Username"
             value={userName}
             onChange={(e) => SetUserName(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
+        <div class="group-input">
           <label>Email</label>
           <input
             type="email"
             placeholder="Entry your email"
             value={email}
             onChange={(e) => SetEmail(e.target.value)}
+            className="input"
           />
         </div>
-        <div>
+        <div class="group-input">
           <label>Password</label>
           <input
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="input"
           />
         </div>
       </div>
-      <button type="submit">Register</button>
-      <Link to="/login">Login</Link>
+      <button className="btn" type="submit">
+        Register
+      </button>
+      <div className="link">
+        <p class="signup-link"> Already registered?</p>
+        <Link to="/login">Login</Link>
+      </div>
     </form>
   );
 };
